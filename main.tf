09884bc1c24526a5f228c570d515e8f0008619e9
Zeyad Essam <zeyadabdelhakeem1@gmail.com>
2024-04-12 22:59:40 +0200

    ec2

--- main.tf
+++ main.tf
@@ -95,7 +95,19 @@ output "aws_ami_id" {
 }
 
 
-# resource "aws_instance" "myapp-server" {
-#   ami = data.aws_ami.latest-amazon-linux-image.id
+resource "aws_instance" "myapp-server" {
+  ami = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_type
 
-# }
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+  availability_zone = var.avail_zone
+  associate_public_ip_address = true
+
+  #key_name = "myapp-key"
+
+  tags = {
+    Name = "${var.env_prefix}-server"
+  } 
+}
+
--- variables.tf
+++ variables.tf
@@ -3,3 +3,4 @@ variable subnet_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
 # variable my_ip {}
+variable instance_type {}
