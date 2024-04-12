4d59b64243466b7169910f2151c751387fc144a6
Zeyad Essam <zeyadabdelhakeem1@gmail.com>
2024-04-12 22:12:14 +0200

    adding-rtb-igw-association

--- main.tf
+++ main.tf
@@ -1,9 +1,6 @@
 provider "aws" {}
 
-variable vpc_cidr_block {}
-variable subnet_cidr_block {}
-variable avial_zone {}
-variable env_prefix {}
+
 
 
 resource "aws_vpc" "myapp-vpc" {
@@ -16,9 +13,34 @@ resource "aws_vpc" "myapp-vpc" {
 resource "aws_subnet" "myapp-subnet-1" {
     vpc_id = aws_vpc.myapp-vpc.id
     cidr_block = var.subnet_cidr_block
-    availability_zone = var.avial_zone
+    availability_zone = var.avail_zone
     tags = {
         Name = "${var.env_prefix}-subnet"
     }
 }
 
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  tags = {
+     Name = "${var.env_prefix}-igw"
+   }
+}
+
+
+resource "aws_route_table" "myapp-route-table" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+
+  tags = {
+     Name = "${var.env_prefix}-rtb"
+   }
+}
+
+resource "aws_route_table_association" "a-rtb-subnet" {
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.myapp-route-table.id
+}
