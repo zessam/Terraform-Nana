5301d86c608e6a2d7e4b7202383930656f2426cb
Zeyad Essam <zeyadabdelhakeem1@gmail.com>
2024-04-12 22:34:54 +0200

    sg

--- main.tf
+++ main.tf
@@ -44,3 +44,35 @@ resource "aws_route_table_association" "a-rtb-subnet" {
   subnet_id = aws_subnet.myapp-subnet-1.id
   route_table_id = aws_route_table.myapp-route-table.id
 }
+
+resource "aws_security_group" "myapp-sg" {
+    name = "myapp-sg"
+    vpc_id = aws_vpc.myapp-vpc.id
+
+    ingress {
+      from_port = 22
+      to_port = 22 
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+      # cidr_blocks = [var.my_ip]
+    }
+
+    ingress {
+      from_port = 8080
+      to_port = 8080
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+      from_port = 8080
+      to_port = 8080
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+
+    }
+
+    tags = {
+     Name = "${var.env_prefix}-sg"
+   }
+}
